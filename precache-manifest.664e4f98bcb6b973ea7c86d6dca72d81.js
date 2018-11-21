e4a2358b4718eefcc972201fa19373a62385fdc8
Анна Петько <annapetko@icloud.com>
2018-11-21 18:58:43 +0100

    Updates

---
+++ precache-manifest.664e4f98bcb6b973ea7c86d6dca72d81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6fb593b5ae9b570acc1",
+    "url": "/assignment/static/js/runtime~main.b6fb593b.js"
+  },
+  {
+    "revision": "873ac3e85db34a992802",
+    "url": "/assignment/static/js/main.873ac3e8.chunk.js"
+  },
+  {
+    "revision": "0d2fb406b4b1f5ee4df0",
+    "url": "/assignment/static/js/1.0d2fb406.chunk.js"
+  },
+  {
+    "revision": "873ac3e85db34a992802",
+    "url": "/assignment/static/css/main.188eff2d.chunk.css"
+  },
+  {
+    "revision": "bb5e60fd87878c7686729cf29ac007cd",
+    "url": "/assignment/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-const initialCity = 'Moscow';
-
-ReactDOM.render(<App initialCity={initialCity}/>, document.getElementById('root'));
